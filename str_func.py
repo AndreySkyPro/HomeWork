bdede52548635f0a101936bc93f9f9d3d3d7d7e4
Andrey <bmw_740@mail.ru>
2023-03-20 22:10:51 +0500

    added new function to tile input and change docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,11 @@
-#new file
+# new file
 def convert_str():
-    """Возвращает все символы в верхнем реестре"""
+    """Возвращает все символы в очень верхнем реестре"""
     user_input = str(input())
     return user_input.upper()
 
+
+def convert_title_str():
+    """Возвращает Все Слова С Заглавной Буквы"""
+    user_input = str(input())
+    return user_input.title()
